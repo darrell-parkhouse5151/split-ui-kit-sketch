410156d65bac007f95ee8aab2e5694787e5cedca
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-13 15:19:58 -0500

    using es6 classes

--- js/login.js
+++ js/login.js
@@ -1,5 +1,29 @@
-$(document).ready(function() {
-    var body = $('body');
+class Login {
+    init() {
+        this.cacheDom();
+        this.handleEvents();
+    }
 
-    body.addClass('login');
-});
+    cacheDom() {
+        this.body = $("body");
+        this.loginForm = $('.login-form');
+
+    }
+
+    handleEvents() {
+        let _self = this;
+
+        _self.body.addClass('login');
+
+        _self.loginForm.on('submit', (e) => {
+            e.preventDefault();
+
+            $.ajax({
+                type: 'post'
+            })
+        })
+    }
+}
+
+let login = new Login();
+login.init();
